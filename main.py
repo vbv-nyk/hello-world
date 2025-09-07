7c65811b6664a54273aa257aad6cd399735a4055
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-09-07 12:39:42 +0530

    remove files, tryig to do something else now

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-print("Adding to system path")
-import sys
-print(sys.path)
-sys.path.append("./libs")
 from modules.fetcher import Fetcher
 from modules.reporter import Reporter
 
-
 print("Fetching sample data from JSONPlaceholder API...")
-
 fetcher = Fetcher("https://jsonplaceholder.typicode.com/todos")
 data = fetcher.fetch()
 print(f"Fetched {len(data)} items.")
-
 reporter = Reporter(data)
 reporter.print_report()
