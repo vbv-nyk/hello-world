440de527450398958626313ddd96b35b4b0d087e
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-09-07 11:23:35 +0530

    I guess it's working, but you're probably confused why I'mm doing all this

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from modules.fetcher import Fetcher
 from modules.reporter import Reporter
 
 
-def run():
-    print("Fetching sample data from JSONPlaceholder API...")
+print("Fetching sample data from JSONPlaceholder API...")
 
-    fetcher = Fetcher("https://jsonplaceholder.typicode.com/todos")
-    data = fetcher.fetch()
-    print(f"Fetched {len(data)} items.")
+fetcher = Fetcher("https://jsonplaceholder.typicode.com/todos")
+data = fetcher.fetch()
+print(f"Fetched {len(data)} items.")
 
-    reporter = Reporter(data)
-    reporter.print_report()
-
-if __name__ == "__main__":
-    run()
+reporter = Reporter(data)
+reporter.print_report()
