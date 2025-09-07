0d62ce48ec0dfbd24e5fb4b6335ffb78ec7e8bc8
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-09-07 21:16:38 +0530

    New test

---
+++ modules/formulaai/helpers.py
@@ -0,0 +1,36 @@
+from com.priveasy.formulaai.builtin import BatteryHelper, DeviceHelper, NetworkHelper, \
+    StorageHelper, LocaleHelper, DisplayHelper, AppHelper, ClipboardHelper
+
+class PyBattery:
+    def get_level(self): return BatteryHelper.getBatteryLevel()
+    def is_charging(self): return BatteryHelper.isCharging()
+
+class PyDevice:
+    def get_model(self): return DeviceHelper.getDeviceModel()
+    def get_android_version(self): return DeviceHelper.getAndroidVersion()
+    def get_manufacturer(self): return DeviceHelper.getManufacturer()
+
+class PyNetwork:
+    def is_online(self): return NetworkHelper.isOnline()
+
+class PyStorage:
+    def free_space(self): return StorageHelper.getInternalFreeSpace()
+    def total_space(self): return StorageHelper.getInternalTotalSpace()
+    def is_external_mounted(self): return StorageHelper.isExternalStorageMounted()
+
+class PyLocale:
+    def get_language(self): return LocaleHelper.getLanguage()
+    def get_country(self): return LocaleHelper.getCountry()
+
+class PyDisplay:
+    def get_resolution(self): return DisplayHelper.getScreenResolution()
+    def get_density(self): return DisplayHelper.getDensityDpi()
+
+class PyApp:
+    def get_package(self): return AppHelper.getPackageName()
+    def get_version_name(self): return AppHelper.getVersionName()
+    def get_version_code(self): return AppHelper.getVersionCode()
+
+class PyClipboard:
+    def set_text(self, text): ClipboardHelper.setText(text)
+    def get_text(self): return ClipboardHelper.getText()
