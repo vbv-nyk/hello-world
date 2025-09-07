abed9a39c73da7335b9a39b0778d51cc13ae1d88
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-09-07 19:43:39 +0530

    Huge drama

--- modules/pybattery.py
+++ modules/pybattery.py
@@ -1,8 +1,12 @@
 from com.priveasy.formulaai.builtin import BatteryHelper as KBatteryHelper
 
 class PyBattery:
+    def __init__(self):
+        # Access the singleton instance
+        self._kt = KBatteryHelper.INSTANCE
+
     def get_battery_level(self):
-        return KBatteryHelper.getBatteryLevel()
+        return self._kt.getBatteryLevel()
 
     def is_charging(self):
-        return KBatteryHelper.isCharging()
+        return self._kt.isCharging()
